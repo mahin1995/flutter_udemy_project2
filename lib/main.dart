51ed20ced1feee644fe166f7f7ca946c63375b40
Mohaimanul islam Mahin <56087765+Mahin14@users.noreply.github.com>
2021-08-17 13:02:09 +0600

    from office pc

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final List<Transaction> transaction = [
+    Transaction(
+        id: 't1', title: "New showes", amount: 69.3, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: "Growsery", amount: 999.3, date: DateTime.now()),
+  ];
+  // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: Container(width: 100,height: 200,child: Text("Chart")),
-          elevation: 5, 
-          ),//card
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                child: Container(
+                    width: double.infinity,
+                    height: 200,
+                    color: Colors.blue,
+                    child: Text("Chart")),
+                elevation: 5,
+              ),
+            ), //card
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
+            ),
           
-           Card(
-            child: Text("List of chart"),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
+
+
 }
